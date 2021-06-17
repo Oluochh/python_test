0e5bc2ea2cc9c00e82668270567ac24b035c1219
oluochh <gillyoluochh@gmail.com>
2021-06-17 10:51:35 +0300

    first commit

---
+++ python_test.py
@@ -0,0 +1,47 @@
+x=[100,110,120,130,140,150]
+y=[]
+for a in x:
+        b=a*5
+        y.append(b)
+print(y) 
+
+y=divisible_by_three[27] 
+x=[]
+x=[x/3 for a in y]
+print(x) 
+
+
+
+x=[[1,2],[3,4],[5,6]]
+z=set(x)
+print(type(x))
+
+
+z=smallest([10,9,8,7,6,5,3,2,1])
+z.smallest[]
+
+
+x=['a','b','a','e','d','b,','c','e','f','g','h']
+y=set(x)
+print(y)
+
+
+students=[{"age":19,"name":"Eunice"},{"age":21,"name":"Agnes"},{"age":18,"name":"Teresa"},{"age":22,"name":"Asha"}]
+for student in students:
+        name=students['name']
+        age=students['age']
+sentence=" Hello {} you were born in the year{}".format(name,age)
+print(sentence)
+
+Class Rectangle:
+    def __init__(self,width,length):
+        self.width=width
+        self.length=length
+    def Method(self):
+      return     
+
+
+
+
+
+        
